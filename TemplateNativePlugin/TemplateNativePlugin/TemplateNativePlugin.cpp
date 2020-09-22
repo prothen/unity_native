7c4161f01beecf2ea1313e59642cfbeae0461a9a
Philipp Rothenhäusler <phirot@kth.se>
2020-09-22 18:49:30 +0200

    Update TemplateNativePlugin dll

--- TemplateNativePlugin/TemplateNativePlugin/TemplateNativePlugin.cpp
+++ TemplateNativePlugin/TemplateNativePlugin/TemplateNativePlugin.cpp
@@ -4,10 +4,6 @@
 #include "pch.h"
 #include "framework.h"
 #include "TemplateNativePlugin.h"
-//
-#include <stdio.h>
-#include <conio.h>
-#include <Python.h>
 
 Test* initialise() {
 	Test* persistentObject = new Test();
@@ -17,21 +13,6 @@ Test* initialise() {
 float update(Test* persistentObject) {
 	persistentObject->increment();
 	return persistentObject->get_value();
-}
-
-int launch() {
-		// c:\\users\\colma\\hcps\\Interfaces\\Python\\
-		
-		char filename[] = "client.py";
-		FILE* fp;
-
-		Py_Initialize();
-
-		fp = _Py_fopen(filename, "r");
-		PyRun_SimpleFile(fp, filename);
-
-		Py_Finalize();
-		return 0;
 }
 
 // This is an example of an exported variable
